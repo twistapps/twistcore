77451b31df80db0c1f05ece435ebc507fbcfcfac
bloomstick <twistappscontact@gmail.com>
2022-10-22 20:14:42 +0300

    feat: Package versioning utils

---
+++ Scripts/PackageRegistry/Versioning/Version.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+public class Version
+{
+    private readonly uint[] versionRaw;
+            
+    public uint Major => versionRaw[0];
+    public uint Minor => versionRaw[1];
+    public uint Patch => versionRaw[2];
+
+    public string version => string.Join(".", versionRaw.Select(v => v.ToString()));
+
+    public Version(string version)
+    {
+        versionRaw = version.Split('.').Select(uint.Parse).ToArray();
+    }
+
+    public override string ToString()
+    {
+        return version;
+    }
+}
---
+++ Scripts/PackageRegistry/Versioning/VersionComparison.cs
@@ -0,0 +1,39 @@
+﻿public class VersionComparison
+{
+    private const string ZERO = "0.0.0";
+    private readonly Version _current, _next;
+
+    public bool HasMajorUpdate()
+    {
+        return _current.Major < _next.Major;
+    }
+
+    public bool HasMinorUpdate()
+    {
+        return HasMajorUpdate() || _current.Minor < _next.Minor;
+    }
+
+    public bool HasPatchUpdate()
+    {
+        return HasMajorUpdate() || HasMinorUpdate() || _current.Patch < _next.Patch;
+    }
+
+    public bool hasUpdate => HasPatchUpdate();
+    
+    public VersionComparison(string currentVersion, string nextVersion)
+    {
+        _current = new Version(currentVersion);
+        _next = new Version(nextVersion);
+    }
+
+    public VersionComparison()
+    {
+        _current = new Version(ZERO);
+        _next = new Version(ZERO);
+    }
+    
+    public override string ToString()
+    {
+        return _current.ToString();
+    }
+}
