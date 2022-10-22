1ffcb6d1747d06ac9d7fcc01d1b2bcbb2d965e83
bloomstick <twistappscontact@gmail.com>
2022-10-22 20:15:24 +0300

    feat: Core Settings asset

---
+++ Scripts/Settings/TwistCoreSettings.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwistCore
+{
+    public class TwistCoreSettings : SettingsAsset
+    {
+        public bool enablePackageCreation;
+        public string newPackageName;
+        public string newPackageDisplayname;
+        public string newPackageOrganizationName = "twistapps";
+        public string newPackageVersion = "0.0.1";
+        public string newPackageDescription;
+        
+        public override string GetEditorWindowTitle()
+        {
+            return "Twist Core";
+        }
+
+        public override string GetPackageName()
+        {
+            return "com.twistapps.twistcore";
+        }
+    }
+}
