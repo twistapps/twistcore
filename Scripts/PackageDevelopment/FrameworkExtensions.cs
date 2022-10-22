ac681fba6b1b09a40aa15a6dfdd3c1f45cc678d3
bloomstick <twistappscontact@gmail.com>
2022-10-22 20:44:53 +0300

    feat: FW Extensions for PackageInfo/PackageData

---
+++ Scripts/PackageDevelopment/FrameworkExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace TwistCore
+{
+    public static class FrameworkExtensions
+    {
+        public static string Alias(this PackageInfo package)
+        {
+            return package.name.Split('.').LastOrDefault();
+        }
+        
+        public static string Alias(this PackageData package)
+        {
+            return package.name.Split('.').LastOrDefault();
+        }
+
+        public static string Asmdef(this PackageInfo package)
+        {
+            var packageFolder = package.assetPath;
+            return Directory.GetFiles(packageFolder).FirstOrDefault(file => file.ToLower().EndsWith(package.Alias() + ".asmdef"));
+        }
+        
+        public static string Asmdef(this PackageData package)
+        {
+            var packageFolder = package.assetPath;
+            return Directory.GetFiles(packageFolder).FirstOrDefault(file => file.ToLower().EndsWith(package.Alias() + ".asmdef"));
+        }
+        
+        public static GUID AsmdefGuid(this PackageInfo package)
+        {
+            return AssetDatabase.GUIDFromAssetPath(Asmdef(package));
+        }
+        
+        public static GUID AsmdefGuid(this PackageData package)
+        {
+            return AssetDatabase.GUIDFromAssetPath(Asmdef(package));
+        }
+    }
+}
