9ab1bd9f7a659c9b2e061e66f90305a6102fcfc9
bloomstick <twistappscontact@gmail.com>
2022-10-22 20:12:05 +0300

    feat: Create commonly used GUIStyles

---
+++ Scripts/Editor/GUIStyles.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TwistCore.Editor
+{
+    public static class GUIStyles
+    {
+        public static readonly GUIStyle DefaultLabel = EditorStyles.label;
+        
+        public static readonly GUIStyle SuccessLabel = new GUIStyle(EditorStyles.label)
+        {
+            normal =
+            {
+                textColor = Color.green
+            }
+        };
+
+        public static readonly GUIStyle FailureLabel = new GUIStyle(EditorStyles.label)
+        {
+            normal =
+            {
+                textColor = Color.red
+            }
+        };
+            
+        public static readonly GUIStyle WarningLabel = new GUIStyle(EditorStyles.label)
+        {
+            normal =
+            {
+                textColor = Color.yellow
+            }
+        };
+
+        public const string IconSuccess = "d_FilterSelectedOnly";
+        public const string IconError = "console.erroricon.sml@2x";
+        public const string IconWarning = "console.warnicon.sml@2x";
+        public const string IconInfo = "console.infoicon.sml@2x";
+    }
+}
