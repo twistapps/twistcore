5cb8bb055ddaacb0f99cb71696e7f20d3770f413
bloomstick <twistappscontact@gmail.com>
2022-10-27 06:11:50 +0300

    feat: PersistentEditorData singleton

---
+++ Scripts/Settings/Editor/PersistentEditorData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TwistCore
+{
+    public class PersistentEditorData : ScriptableSingleton<PersistentEditorData>
+    {
+        [SerializeField] private bool gitInitialized;
+        [SerializeField] private bool gitAvailable;
+        [SerializeField] private string gitVersion;
+
+        [SerializeField] private VersionComparison _coreUpdateInfo;
+        public VersionComparison CoreUpdateInfo => _coreUpdateInfo ??= FetchCoreUpdates();
+
+        public bool GitAvailable => gitInitialized ? gitAvailable : InitializeGit() != null;
+        public string GitVersion => gitInitialized ? gitVersion : InitializeGit();
+
+        private static VersionComparison FetchCoreUpdates()
+        {
+            var package = PackageRegistry.Get(TwistCore.PackageName);
+            return GithubVersionControl.FetchUpdates(package);
+        }
+
+        private string InitializeGit()
+        {
+            try
+            {
+                gitVersion = GitCmd.GetVersion();
+                gitAvailable = true;
+            }
+            catch
+            {
+                gitVersion = null;
+                gitAvailable = false;
+            }
+
+            gitInitialized = true;
+            return gitVersion;
+        }
+    }
+}
