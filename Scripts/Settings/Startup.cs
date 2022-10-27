f2eedd8b5acf7001684dff0b99a26e71cb088ac8
bloomstick <twistappscontact@gmail.com>
2022-10-27 06:12:23 +0300

    fix: Moved to PersistentEditorData

--- Scripts/Settings/Startup.cs
+++ Scripts/Settings/Startup.cs
@@ -3,26 +3,25 @@ using UnityEngine;
 
 namespace TwistCore
 {
-    [InitializeOnLoad]
-    public class Startup
-    {
-        public static PackageData PackageData;
-        public static readonly bool GitAvailable;
-        public static readonly string GitVersion;
-        
-        static Startup()
-        {
-            try
-            {
-                GitVersion = GitCmd.GetVersion();
-                GitAvailable = true;
-            }
-            catch
-            {
-                GitAvailable = false;
-            }
-            //packageInfo = PackageFetch.Get()
-        }
-    }
+    // [InitializeOnLoad]
+    // public class Startup
+    // {
+    //     public static readonly bool GitAvailable;
+    //     public static readonly string GitVersion;
+    //     
+    //     static Startup()
+    //     {
+    //         try
+    //         {
+    //             GitVersion = GitCmd.GetVersion();
+    //             GitAvailable = true;
+    //         }
+    //         catch
+    //         {
+    //             GitAvailable = false;
+    //         }
+    //         //packageInfo = PackageFetch.Get()
+    //     }
+    // }
 
 }
