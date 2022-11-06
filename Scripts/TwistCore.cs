48a781f6a9ab5afea643b95295194a4531f4405c
bloomstick <twistappscontact@gmail.com>
2022-11-07 02:26:04 +0300

    feat: New Definitions

--- Scripts/TwistCore.cs
+++ Scripts/TwistCore.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using TwistCore.Utils;
+using UnityEditor.PackageManager;
 
 // ReSharper disable once CheckNamespace
 namespace TwistCore
@@ -9,6 +10,13 @@ namespace TwistCore
         internal static string PackageName => SettingsUtility.Load<TwistCoreSettings>().GetPackageName();
 
         internal static string PackageTemplateFolder => Path.Combine("Packages", PackageName, ".NewPackageTemplate");
+        internal static string SettingsFolder => Path.Combine("Assets", "TwistApps", "Resources", "Settings");
+
+        internal static string ManifestPath => PackageRegistry.Get(PackageName).source == PackageSource.Embedded 
+            ? Path.Combine("Packages", PackageName, ManifestFilename) 
+            : Path.Combine("Assets", "TwistApps", "Resources", ManifestFilename);
+
+        internal const string ManifestFilename = "package-manifest.json";
 
         internal static string PackageRegistryNameMask =>
             Path.Combine("Packages", PackageName, ".package-registry-mask");
