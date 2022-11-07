e32579aa378dd536f8f77c775e2f8f7a7bab37a8
bloomstick <twistappscontact@gmail.com>
2022-11-07 07:34:28 +0300

    fix: Convert PackageData to PackageInfo

--- Scripts/PackageDevelopment/PackageData.cs
+++ Scripts/PackageDevelopment/PackageData.cs
@@ -4,28 +4,57 @@
 // ReSharper disable UnassignedField.Global
 
 using System;
+using TwistCore.PackageRegistry.Versioning;
 using UnityEditor.PackageManager;
 
-namespace TwistCore
+namespace TwistCore.PackageDevelopment
 {
     [Serializable]
     public class PackageData
     {
-        private Version _versionInfo;
-
         public string assetPath;
-
-        public Author author;
         public string description;
         public string displayName;
 
         public string[] keywords;
+
         public string name;
 
-        public Repository repository;
         //public string unity;
         public string version;
-        public Version versionInfo => _versionInfo ??= new Version(version);
+        private VersionInfo _versionInfoInfo;
+
+        public Author author;
+
+        public Repository repository;
+        public VersionInfo VersionInfoInfo => _versionInfoInfo ??= new VersionInfo(version);
+
+        public static explicit operator PackageData(PackageInfo info)
+        {
+            if (info == null) return null;
+            var data = new PackageData
+            {
+                author = new Author
+                {
+                    email = info.author.email,
+                    name = info.author.name,
+                    url = info.author.url
+                },
+                description = info.description,
+                displayName = info.displayName,
+                keywords = info.keywords,
+                name = info.name,
+                repository = new Repository
+                {
+                    type = info.repository.type,
+                    url = info.repository.url
+                },
+                version = info.version,
+                assetPath = info.assetPath
+            };
+
+            return data;
+        }
 
         public class Repository
         {
@@ -39,27 +68,5 @@ namespace TwistCore
             public string name;
             public string url;
         }
-
-        public static explicit operator PackageData(PackageInfo info) => new PackageData
-        {
-            author = new Author
-            {
-                email = info.author.email,
-                name = info.author.name,
-                url = info.author.url
-            },
-            description = info.description,
-            displayName = info.displayName,
-            keywords = info.keywords,
-            name = info.name,
-            
-            repository = new Repository
-            {
-                type = info.repository.type,
-                url = info.repository.url
-            },
-            version = info.version,
-            assetPath = info.assetPath
-        };
     }
 }
