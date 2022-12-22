971bf7c3345d545a3f2cb4b90430a0e76c8e2496
bloomstick <twistappscontact@gmail.com>
2022-12-22 04:12:51 +0300

    feat: Collect ScriptTemplates from supported pkgs

---
+++ Scripts/CodeGen/TemplateFoldersWatcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TwistCore.CodeGen.Editor;
+using TwistCore.DependencyManagement;
+using TwistCore.PackageRegistry;
+using TwistCore.ProgressWindow.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace TwistCore.CodeGen
+{
+    public static class TemplateFoldersWatcher
+    {
+        [MenuItem("Tools/Twist Apps/Commands/Sync CodeGen Templates")]
+        public static void SyncTemplateFoldersFromAllRegisteredPackages()
+        {
+            const string templateFolderSlug = "ScriptTemplates";
+            var sourceFolders = PackageRegistryUtils.Collection.Select(
+                package => Path.Combine(package.assetPath, templateFolderSlug))
+                .Where(Directory.Exists)
+                .ToArray();
+            
+            TaskManager.Enqueue(
+                FolderSync.SyncFoldersTask(CodeGenDefinitions.GeneratedFolder, sourceFolders), 
+                "Gathering templates");
+        }
+    }
+}
