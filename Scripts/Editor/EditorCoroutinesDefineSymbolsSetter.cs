65e954c20a43078b4aa77e0a3bb6a958e4940630
bloomstick <twistappscontact@gmail.com>
2022-12-23 07:57:18 +0300

    feat: EditorCoroutines define symbols setter (#10)

---
+++ Scripts/Editor/EditorCoroutinesDefineSymbolsSetter.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using TwistCore.PackageRegistry;
+using UnityEditor;
+using UnityEditor.PackageManager;
+
+namespace TwistCore.Editor
+{
+    public static class EditorCoroutinesDefineSymbolsSetter
+    {
+        private const string EditorCoroutines = "EDITOR_COROUTINES";
+        private const string PackageName = "com.unity.editorcoroutines";
+        
+        [InitializeOnLoadMethod]
+        private static void CheckEditorCoroutinesInstalled()
+        {
+            if (UPMCollection.GetFromAllPackages(PackageName) == null)
+                ScriptingDefinesSetter.RemoveSymbols(EditorCoroutines);
+            else
+                ScriptingDefinesSetter.AddSymbols(EditorCoroutines);
+        }
+        
+        [InitializeOnLoadMethod]
+        private static void SubscribeToUPMEvent()
+        {
+            // This causes the method to be invoked after the Editor registers the new list of packages.
+            Events.registeringPackages += RegisteringPackagesEventHandler;
+        }
+        
+        public static void RegisteringPackagesEventHandler(PackageRegistrationEventArgs packageRegistrationEventArgs)
+        {
+            if (packageRegistrationEventArgs.removed.FirstOrDefault(pkg => pkg.name == PackageName) != null)
+            {
+                ScriptingDefinesSetter.RemoveSymbols(EditorCoroutines);
+                return;
+            }
+            
+            if (packageRegistrationEventArgs.added.FirstOrDefault(pkg => pkg.name == PackageName) != null)
+            {
+                ScriptingDefinesSetter.AddSymbols(EditorCoroutines);
+            }
+        }
+    }
+}
