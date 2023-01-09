b5456f1a9e207de3a9cf86b8e58444623acf5dc8
bloomstick <twistappscontact@gmail.com>
2023-01-09 10:01:11 +0300

    feat: Select pkg in project view widget

---
+++ Scripts/Editor/UIComponents/SelectPackageWidget.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace TwistCore.Editor.GuiWidgets
+{
+    public class SelectPackageWidget<T> : GuiWidget<T> where T : SettingsAsset
+    {
+        public override void Draw()
+        {
+            Window.ButtonLabel("Focus project view on this package", new Button("Ping", () =>
+            {
+                Selection.activeObject =
+                    AssetDatabase.LoadAssetAtPath<Object>(Path.Combine("Packages", PackageName, "package.json"));
+            }));
+        }
+    }
+}
