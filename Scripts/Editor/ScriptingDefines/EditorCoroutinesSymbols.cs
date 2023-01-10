d7ab7f959ab94400664c5fa4bb20381bd6c93679
bloomstick <twistappscontact@gmail.com>
2023-01-10 03:17:24 +0300

    feat: Symbols setter rewrite for editorcoroutines

---
+++ Scripts/Editor/ScriptingDefines/EditorCoroutinesSymbols.cs
@@ -0,0 +1,20 @@
+﻿using TwistCore.PackageRegistry.Editor;
+
+namespace TwistCore.Editor
+{
+    [PackageName(PackageName)]
+    public class EditorCoroutinesSymbols : ConditionalDefineSymbols
+    {
+        private const string EDITOR_COROUTINES = "EDITOR_COROUTINES";
+        private const string PackageName = "com.unity.editorcoroutines";
+        public override string GetSymbols()
+        {
+            return EDITOR_COROUTINES;
+        }
+
+        public override bool ShouldSetDefines()
+        {
+            return UPMCollection.GetFromAllPackages(PackageName) != null;
+        }
+    }
+}
---
+++ Scripts/Editor/ScriptingDefines/PackageNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwistCore.Editor
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PackageNameAttribute : Attribute
+    {
+        public readonly string PackageName;
+
+        public PackageNameAttribute(string packageName)
+        {
+            PackageName = packageName;
+        }
+    }
+}
